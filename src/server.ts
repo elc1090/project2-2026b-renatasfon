7e1170a89e33ff4f1c679fe95c91bfbc9570b0de
renata <renata.sfon@gmail.com>
2026-09-15 19:54:50 -0300

    Configura conexão do Prisma com PostgreeSQL

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import prisma from "./prisma.js";
 
 const app = express();
 
@@ -8,6 +9,12 @@ app.get("/", (req, res) => {
     res.json({
         mensagem: "API Doe Escola funcionando!"
     });
+});
+
+app.get("/usuarios", async (req, res) => {
+    const usuarios = await prisma.usuario.findMany();
+
+    res.json(usuarios);
 });
 
 app.listen(3000, () => {
