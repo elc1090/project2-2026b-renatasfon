49bfd328bdc5b6b6a7149ae553e8e8ce9aac7b25
renata <renata.sfon@gmail.com>
2026-09-15 21:10:40 -0300

    Tratamento de erro com Prisma

--- src/controllers/usuarios.controller.ts
+++ src/controllers/usuarios.controller.ts
@@ -1,10 +1,17 @@
 import type { Request, Response } from "express";
+import { Prisma } from "../../generated/prisma/client.js";
 import prisma from "../prisma.js";
 
 export async function listarUsuarios(req: Request, res: Response) {
-    const usuarios = await prisma.usuario.findMany();
+    try {
+        const usuarios = await prisma.usuario.findMany();
 
-    res.json(usuarios);
+        res.json(usuarios);
+    } catch (erro) {
+        return res.status(500).json({
+            erro: "Erro interno do servidor."
+        });
+    }
 }
 
 export async function criarUsuario(req: Request, res: Response) {
@@ -15,6 +22,12 @@ export async function criarUsuario(req: Request, res: Response) {
             erro: "Nome e tipo são obrigatórios."
         });
     }
+
+    if (tipo !== "ALUNO" && tipo !== "DOADOR") {
+        return res.status(400).json({
+            erro: "Tipo de usuário inválido. Use ALUNO ou DOADOR."
+    });
+}
 
     // se aluno não possui email
     if (tipo === "ALUNO" && email) {
@@ -30,13 +43,28 @@ export async function criarUsuario(req: Request, res: Response) {
         });
     }
 
-    const usuario = await prisma.usuario.create({
-        data: {
-            nome,
-            email,
-            tipo,
-        },
-    });
+    try {
+        const usuario = await prisma.usuario.create({
+            data: {
+                nome,
+                email: email || null,
+                tipo,
+            },
+        });
 
-    res.status(201).json(usuario);
+        res.status(201).json(usuario);
+    } catch (erro) {
+            if (
+                erro instanceof Prisma.PrismaClientKnownRequestError &&
+                erro.code === "P2002"
+            ) {
+                return res.status(409).json({
+                    erro: "Este email já está cadastrado."
+                });
+            }
+
+            return res.status(500).json({
+                erro: "Erro interno do servidor."
+            });
+        }
 }
